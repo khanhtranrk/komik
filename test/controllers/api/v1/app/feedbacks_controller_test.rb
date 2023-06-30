e8675e4498baf3c3d303c6f9d6b3200117249ffa
duykhanhrk <tranduykhanh.rk@gmail.com>
2023-06-30 16:50:47 +0700

    ref: remove trash

--- test/controllers/api/v1/app/feedbacks_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Api::V1::App::FeedbacksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/api/v1/app/plans_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Api::V1::App::PlansControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
