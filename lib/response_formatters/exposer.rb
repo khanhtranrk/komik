6d1816d8106a32ef70b145c379627f206065775b
duykhanhrk <tranduykhanh.rk@gmail.com>
2023-07-01 16:23:23 +0700

    ref: exposer

--- lib/response_formatters/exposer.rb
+++ lib/response_formatters/exposer.rb
@@ -4,7 +4,7 @@ module ResponseFormatters
   module Exposer
     def expose(resource = nil, options = {})
 
-      data = if resource.present? && (options.key?(:serializer) || options.key?(:each_serializer))
+      data = if resource && (options.key?(:serializer) || options.key?(:each_serializer))
                options[:adapter] ||= :json
                ActiveModelSerializers::SerializableResource.new(resource, options)
              else
